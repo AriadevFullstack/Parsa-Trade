ffedb78c1c7d9deef4f41408d18298ec27227be0
Ariadesigner <m.sadegh.noruzi7165@gmail.com>
2025-07-20 10:23:17 -0700

    Initial commit  parsa trade frontend

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://persa-trade-frontend.onrender.com', 'http://localhost:3000'],
+  credentials: true,
+}));
+
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
